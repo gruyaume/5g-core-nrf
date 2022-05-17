2dcc24ec7f0ff69fd77bc2ba71c5a3726344972e
guillaume <guillaume.belanger27@gmail.com>
2022-05-17 14:59:09 -0400

    Adds github worflow

--- src/core/config.py
+++ src/core/config.py
@@ -1,7 +1,4 @@
-import secrets
-from typing import Any, Dict, List, Optional, Union
-
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
